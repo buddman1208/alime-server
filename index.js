433f9d95c469dd17a9770f8c295c661309e2870b
KOHA <kotohana5706@edcan.kr>
2016-08-12 18:29:39 +0000

    Configure port, implement testing api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var port = 5000;
+var port = 2000;
 
 var express = require('express');
 var path = require('path');
@@ -12,8 +12,9 @@ var http = require('http').Server(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.post('/', function(req, res){
+  res.send('Testing API').status(200);
+})
 module.exports = app;
 http.listen(port, function(){
     console.log('Alime Server running on Port ' + port);
