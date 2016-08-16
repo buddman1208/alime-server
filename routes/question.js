727f1f6cf72413ff34ee1163c2fc723f279828ff
Junseok Oh <kotohana5706@edcan.kr>
2016-08-16 14:54:32 +0900

    /question/newArticle complete

--- routes/question.js
+++ routes/question.js
@@ -1,5 +1,25 @@
 module.exports = question;
 
 function question(app, db, randomstr) {
-
+    var newArticleParams = ['title', 'date', 'content', 'author', 'password'];
+    var replyArticleParams = ['userid', 'articleid', 'content'];
+    var deleteArticleParams = ['userid', 'articleid']
+    app.post('/question/newArticle', function (req, res) {
+        if (newArticleParams.every(str => req.body[str] != undefined || req.body[str] != null)) {
+            var newArticle = new db.Question({
+                articleid: randomstr.generate(),
+                reply: ''
+            });
+            newArticleParams.forEach(str => newArticle[str] = req.body[str]);
+            newArticle.save(function (err) {
+                if (err) {
+                    console.log(err);
+                    res.sendStatus(500);
+                } else {
+                    res.send(newArticle);
+                    console.log(newArticle);
+                }
+            })
+        } else res.sendStatus(403);
+    });
 }
