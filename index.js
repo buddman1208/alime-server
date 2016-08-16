8196ce30a9ee43473bd96d87115d3fc232c9b20b
Junseok Oh <kotohana5706@edcan.kr>
2016-08-16 14:32:51 +0900

    Refactor : routes -> functional

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 var port = 2000;
 
 var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
 var http = require('http').Server(app);
-
+var mongo = require('./mongo');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/', function(req, res){
-  res.send('Testing API').status(200);
-})
 module.exports = app;
-http.listen(port, function(){
+
+require('./routes/auth')(app, mongo);
+http.listen(port, function () {
     console.log('Alime Server running on Port ' + port);
 });
